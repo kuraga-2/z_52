137166f84247b684f4115c97022a60ff1e5dfcbb
kuraga-2 <denismanarskij@tutamail.com>
2024-10-14 12:27:48 +0300

    z_52

---
+++ main.py
@@ -0,0 +1,15 @@
+input_data = open('input.txt', 'r') 
+data = input_data.read()
+a = str(data)
+a1 = int(data[0])
+a2 = int(data[1])
+a3 = int(data[2])
+b1 = int(data[3])
+b2 = int(data[4])
+b3 = int(data[5])
+if a1 + a2 + a3 == b1 + b2 + b3:
+    output_data = open('output.txt', 'w')
+    output_data.write('YES')
+else:
+    output_data = open('output.txt', 'w')
+    output_data.write('NO')
